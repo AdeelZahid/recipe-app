d43bbec98c7875467300f14fdcbf067ed3a214b5
Adeel Zahid <m.adeel2144@gmail.com>
2020-07-25 01:23:53 +0500

    Setup App.js and updation .
    it's recipe making app .. yumm yumm

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{useEffect, useState} from 'react';
 import './App.css';
 
-function App() {
+const App =() => {
+
+  const [counter, setCounter] = useState(100);
+  const App_Id = '992d87d3';
+  const App_Key = "482c05899eb48b545a107644077aa509";
+  let exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${App_Id}&app_key=${App_Key}`;
+
+  useEffect(() => {
+    console.log("Effect has been RUn .. ")
+  }, [])
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form action="" className="search-form">
+        <input type="text" name="" id="" className="search-bar"/>
+        <button className="search-button"> Search </button>
+      </form>
+        <h1 onClick={() => setCounter(counter + 1)}>{counter}</h1> 
     </div>
-  );
+  )
 }
 
 export default App;
